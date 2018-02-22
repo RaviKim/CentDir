8391273334730330d01514862ff979e6e7fe4046
RaviKim <ravikim3737@gmail.com>
2018-02-22 09:45:23 +0900

    0222.cpp

---
+++ CPlusTest.d/0219.d/0222.d/ShallowCopy1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class CMyData
+{
+	public:
+		CMyData(int nParam)
+		{
+		m_pnData	=	new	int;
+		*m_pnData	=	nParam;
+		}
+
+		int GetData()
+		{
+			if(m_pnData	!=	NULL)
+				return *m_pnData;
+
+			return 0;
+		}
+
+	private:
+		int *m_pnData	=	nullptr;
+};
+
+int main(int argc, char ** argv)
+{
+	CMyData a(10);
+	CMyData	b(a);
+
+	cout << a.GetData()	<<	endl;
+	cout << b.GetData()	<<	endl;
+
+	return 0;
+}
