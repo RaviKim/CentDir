90deb19ab924e4bb67f1cfe1da2d54aafa5da356
RaviKim <ravikim3737@gmail.com>
2018-06-11 17:56:33 +0900

    Daily 10 problem .cpp

---
+++ CPlusTest.d/0611.d/11.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main(void){
+	int count = 0;	// count variable
+	char *SaveArray = new char[100];	// 	make Input char saved array.
+	char *result = new char[100];
+	vector <char> vi;
+	vector <char> ::iterator iter;
+
+
+	cout <<" InPut : " ;
+	scanf("%s" , SaveArray	);
+
+	
+	for(int i = 0; i < 100; i++){
+		if(SaveArray[i] != SaveArray[i+1] ){
+	//	if(SaveArray[i] != SaveArray[i+1] && SaveArray[i+1] !=0){
+			count++;	
+			result[i] = SaveArray[i];	// save & check substring
+			vi.push_back(SaveArray[i]);
+		}
+		else if(SaveArray[i] == SaveArray[i-1]){
+			vi.back();
+		}
+		
+	}
+	cout << " Result is : " ;
+
+	/*
+	for(int i = 0; i < 100; i++){
+		cout << result[i];
+	}
+*/
+	for(iter = vi.begin(); iter != vi.end(); iter++){
+		cout << *iter ; 
+	}
+	cout << endl;
+	
+	
+	cout << " Count ";
+	cout << count-1 << endl;
+
+
+	delete[] SaveArray;
+	delete[] result;
+}
